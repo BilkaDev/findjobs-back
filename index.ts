5ad2603359d7c04d07bd051f39b359e001f7e991
Marcin Bilka <bilkadev@gmail.com>
2022-06-06 13:07:55 +0200

    Added erorr handling.

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import {handleError} from "./utils/errors";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(json());
+
+
+
+// ROUTES
+
+
+
+
+app.use(handleError)
+
+app.listen(3001,'0.0.0.0', ()=> {
+    console.log('listening on http://localhost:3001')
+});
