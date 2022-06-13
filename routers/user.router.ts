4c468faca80ef98040f5615f218c6b8bd9bdf31e
Marcin Bilka <bilkadev@gmail.com>
2022-06-14 00:06:22 +0200

    Add all EP's to userRouter.

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,9 +1,36 @@
 import {Router} from "express";
+import {UserRecord} from "../records/user.record";
+import {HtmlError} from "../utils/errors";
+import {LoginUserEntity, NewUserEntity} from "../types/user-entity";
 
 export const userRouter = Router();
 
-userRouter.get('/', async (req, res) => {
-    res.json({
-        ok: true,
+userRouter
+    .get('/:id', async (req, res) => {
+        const user = await UserRecord.getUser(req.params.id);
+        if (!user) {
+            throw new HtmlError('Cannot find user.', 404);
+        }
+        res.json({
+            user,
+        });
+    })
+    .post('/login', async (req, res) => {
+        const {email, password} = req.body;
+        const user = new UserRecord({email, password} as LoginUserEntity);
+        const login = await user.login();
+        res.json(
+            login
+        );
+
+    })
+    .post('/singup', async (req, res) => {
+        const user = new UserRecord(req.body as NewUserEntity);
+        await user.singup();
+        const {id, email, name} = user;
+
+        res.status(201)
+            .json({
+                user: {id, email, name}
+            });
     });
-});
