b9ac59764b5d8346f0221a40a047ed7a99a75e93
Marcin Bilka <bilkadev@gmail.com>
2022-06-13 22:02:04 +0200

    Added HtmlError.

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,12 +1,30 @@
 import {NextFunction, Request, Response} from "express";
-export class ValidationError extends Error {}
+
+export class ValidationError extends Error {
+}
+
+export class HtmlError extends Error {
+    code: number;
+
+    constructor(message: string, errorCode: number) {
+        super(message);
+        this.code = errorCode;
+    }
+}
 
 export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
+    console.log("----------------------------------------------------------\n\n", err);
+    if (err instanceof HtmlError) {
+        res
+            .status(err.code)
+            .json({
+                message: err.code === 500 ? 'Sorry, please try again later' : err.message,
+            });
+    }
     res
         .status(err instanceof ValidationError ? 400 : 500)
         .json({
             message: err instanceof ValidationError ? err.message : 'Sorry, please try again later',
-        })
+        });
 
-}
+};
